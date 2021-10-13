ff53935d6b22b0c0f4fae95723cf1575a0df8b8f
yanvovk <55703574+yanvovk@users.noreply.github.com>
2021-10-13 15:39:48 +0300

    Add files via upload

--- ASKOemulator/src/Emulator.java
+++ ASKOemulator/src/Emulator.java
@@ -4,7 +4,7 @@ public class Emulator {
 
     public static Connection getOracleConnection(String hostName, String sid, String userName, String password)
     throws  SQLException {
-        String connectionURL = "jdbc:mercury:oracle:" + hostName + ":1521:" + sid;
+        String connectionURL = "jdbc:mercury:oracle:@" + hostName + ":1521:" + sid;
         Connection connection = DriverManager.getConnection(connectionURL, userName, password);
 
         return connection;
@@ -13,16 +13,16 @@ public class Emulator {
     public static void main(String[] args) throws InterruptedException, SQLException {
         String hostName = "192.168.14.53";
         String sid = "orcl";
-        String userName = "sys";
-        String password = "Aa1234567";
+        String userName = "C##X5";
+        String password = "c##x5";
 
-        Connection dbConnection = getOracleConnection(hostName, sid, userName, password);
         String insertingIntoTask = "INSERT INTO task (id, ticket_id, header, text, client_id, create_date, external_system, sync_mask, contractor_id, state_id, solution_group_id) " +
                 "(SELECT id, id as ticket_id, header, text, applicant_id, create_date, external_system, 0, 1, 1 as state_id, -1 FROM ticket WHERE state_id = -1)";
         String updatingStateId = "UPDATE ticket SET state_id = 1 WHERE id IN (SELECT id FROM task WHERE state_id = 1) AND state_id != 1";
 
         while (true) {
-            try(Statement stmt = dbConnection.createStatement()) {
+            try(Connection dbConnection = getOracleConnection(hostName, sid, userName, password)) {
+                Statement stmt = dbConnection.createStatement();
                 System.out.println("Updated state_id in" + stmt.executeUpdate(updatingStateId) + "tickets");
                 System.out.println("Created" + stmt.executeUpdate(insertingIntoTask) + "tasks");
                 dbConnection.commit();
